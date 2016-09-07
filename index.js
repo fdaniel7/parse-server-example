0fcf51a284a8ae738215db6fe5673d64c1c8765a
fdaniel7 <fdaniel@amdocs.com>
2016-09-07 21:49:56 +0300

    add ynet

--- index.js
+++ index.js
@@ -45,6 +45,12 @@ app.get('/test', function(req, res) {
   res.sendFile(path.join(__dirname, '/public/test.html'));
 });
 
+app.get('/farko', function(req, res) {
+  res.sendFile('http://www.ynet.co.il/home/0,7340,L-8,00.html');
+});
+
+
+
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
 httpServer.listen(port, function() {
